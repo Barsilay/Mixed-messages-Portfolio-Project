6577e06edad77da493e1763c833f906508a82fe7
Barsilay Carrillo <barsilay@icloud.com>
2022-06-06 11:08:39 -0700

    Second Commit (finished javascript section).

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,4 +1,12 @@
 let mixedMessages = ['You can do it!', 'Never give up!', 'Keep your head held high!'];
+
+const newQuote = function(){
+    let randomNumber = Math.floor(Math.random() * (mixedMessages.length));
+    document.getElementById('quoteDisplay').innerHTML = mixedMessages[randomNumber];
+};
+
+
+
 for(let i = 0; i < mixedMessages.length; i++ ) {
     console.log(mixedMessages[i])
 }
